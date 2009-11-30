a895c079e83e9a7e71a94219105367e8a81ccac8
Paul Burt <paul@poptart3.(none)>
2009-11-30 13:03:34 +0100

    updated conf.py

--- mollie/ideal/conf.py
+++ mollie/ideal/conf.py
@@ -21,8 +21,8 @@ MOLLIE_MIN_AMOUNT = getattr(settings, 'MOLLIE_MIN_AMOUNT', 118)
 # A range of logos and buttons can be found at http://www.mollie.nl/support/klanten/logo-badges/
 MOLLIE_IDEAL_BUTTON = getattr(settings, 'MOLLIE_IDEAL_BUTTON', 'http://www.mollie.nl/images/badge-ideal-big.png')
 
-MOLLIE_BTW = getattr(settings, 'MOLLIE_BTW', 19) # integer value
-MOLLIE_TRANSACTION_FEE = getattr(settings, 'MOLLIE_TRANSACTION_FEE', '.99') # string value
+MOLLIE_BTW = getattr(settings, 'MOLLIE_BTW', 19)
+MOLLIE_TRANSACTION_FEE = getattr(settings, 'MOLLIE_TRANSACTION_FEE', '.99')
 
 MOLLIE_TIMEOUT = getattr(settings, 'MOLLIE_TIMEOUT', 10)
 
