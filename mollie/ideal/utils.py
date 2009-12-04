5f2fbb0f89040cb6db4e50c3c8b48ef55f6d6674
Paul Burt <paul@poptart3.(none)>
2009-12-04 09:16:46 +0100

    removed MOLLIE_BANKS_FILE logic
    if we can't connect to Mollie.nl early on then Houston, we have a problem

--- mollie/ideal/utils.py
+++ mollie/ideal/utils.py
@@ -12,8 +12,7 @@ except ImportError:
     except ImportError:
         import xml.etree.ElementTree as etree
 
-from conf import (MOLLIE_TEST, MOLLIE_API_URL, MOLLIE_BANKS_FILE, MOLLIE_TIMEOUT,
-                  MOLLIE_BTW, MOLLIE_TRANSACTION_FEE)
+from conf import MOLLIE_TEST, MOLLIE_API_URL, MOLLIE_TIMEOUT, MOLLIE_BTW, MOLLIE_TRANSACTION_FEE
 
 socket.setdefaulttimeout(MOLLIE_TIMEOUT)
 
@@ -25,57 +24,43 @@ def build_mollie_url(input_dict, base_url=MOLLIE_API_URL, testmode=MOLLIE_TEST):
     url = urlparse.urlunsplit((scheme, netloc, path, query, fragment))
     return url
 
-def parse_mollie_response(file=None, url=None):
-    if file:
-        data = open(file, 'r')
-    elif url:
-        data = urllib2.urlopen(url)
+def parse_mollie_response(url):
+    data = urllib2.urlopen(url)
     mollie_response = etree.parse(data)
-    if file:
-        file.close()
     return mollie_response
 
-def get_mollie_banklist(file=MOLLIE_BANKS_FILE):
-    banklist_dict = {'a': 'banklist'}
-    url = build_mollie_url(banklist_dict)
+def get_mollie_banklist():
+    url = build_mollie_url(dict(a='banklist'))
     try:
-        mollie_response = parse_mollie_response(url=url)
+        mollie_response = parse_mollie_response(url)
     except:
-        mollie_response = parse_mollie_response(file=file)
+        raise
     banks = mollie_response.getiterator('bank')
     choices = ((bank.findtext('bank_id'), bank.findtext('bank_name')) for bank in banks)
     return tuple(choices)
 
 def query_mollie(request_dict, mode=None, valid_modes=('check', 'fetch')):
-    response_dict = {}
-    if mode in valid_modes:
-        request_dict['a'] = mode
-    else:
-        err = "Invalid mode '%s'. Valid modes are '%s' and '%s'." % (mode, valid_modes)
-        raise ValueError(err)
+    assert mode in valid_modes, 'Invalid mode "%s". Valid modes are "%s" and "%s".' % (mode, valid_modes)
+    request_dict['a'] = mode
     url = build_mollie_url(request_dict)
-    mollie_response = parse_mollie_response(url=url)
+    mollie_response = parse_mollie_response(url)
     order = mollie_response.find('order')
+    response_dict = {}
     response_dict['amount'] = decimal.Decimal(order.findtext('amount')) / 100
     response_dict['transaction_id'] = order.findtext('transaction_id')
     if mode == 'fetch':
         response_dict['order_url'] = order.findtext('URL')
     elif mode == 'check':
         response_dict['paid'] = order.findtext('payed') # sic!
-        if response_dict['paid'] == 'true':
-            consumer = order.find('consumer')
-            response_dict['consumerAcount'] = consumer.findtext('consumerAccount')
-            response_dict['consumerCity'] = consumer.findtext('consumerCity')
-            response_dict['consumerName'] = consumer.findtext('consumerName')
+        consumer = order.find('consumer')
+        response_dict['consumerAcount'] = consumer.findtext('consumerAccount')
+        response_dict['consumerCity'] = consumer.findtext('consumerCity')
+        response_dict['consumerName'] = consumer.findtext('consumerName')
     return response_dict
 
 def get_mollie_fee(btw=MOLLIE_BTW, fee=MOLLIE_TRANSACTION_FEE):
-    if not isinstance(btw, int):
-        err = "%s is type '%s'. It should be type 'int'." % (btw, type(btw).__name__)
-        raise TypeError(err)
-    if not isinstance(fee, str):
-        err = "%s is type '%s'. It should be type 'str'." % (fee, type(fee).__name__)
-        raise TypeError(err)
+    assert isinstance(btw, int), 'MOLLIE_BTW must be an integer value'
+    assert isinstance(fee, str), 'MOLLIE_TRANSACTION_FEE must be a string value'
     btw = decimal.Decimal(btw)
     fee = decimal.Decimal(fee)
     fee += ((btw / 100) * fee)
