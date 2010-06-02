ee8f4e6eb74463982d133f6febe0579f641e4fef
Paul Burt <paul.burt@gmail.com>
2010-06-02 11:53:14 +0100

    Fixed last borked commit

---
+++ mollie/ideal/management/commands/get_mollie_banklist.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+
+import os, urllib
+
+from django.core.management.base import NoArgsCommand, CommandError
+
+from mollie.ideal.settings import MOLLIE_API_URL
+
+class Command(NoArgsCommand):
+    help = 'Fetches the latest list of supported banks from Mollie.nl'
+    requires_model_validation = False
+
+    def handle_noargs(self, **options):
+        here = os.path.realpath('.')
+        url = MOLLIE_API_URL + '?a=banklist'
+        file = os.path.join(here, 'mollie_banklist.xml')
+        try:
+            urllib.urlretrieve(url, file)
+            print 'File %s saved to %s' % (os.path.basename(file), here)
+        except:
+            raise CommandError('Something went wrong!')
